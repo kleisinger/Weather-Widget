0282c7fea1e3ca200b511d30cd76abe024e4f61b
Mat Kleisinger <mathew.kleisinger@gmail.com>
2021-12-12 20:39:41 -0600

    add renderCurrentWeather function to populate current conditions in browser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const apiKey = `d0c30f3e5d9786155757451de2759d9c`
 let lat;
 let lon;
+const currentEl = document.querySelector('.current-conditions')
 
 if (!navigator.geolocation) {
   console.log(`Geolocation is not supported by your browser`);
@@ -15,10 +16,23 @@ if (!navigator.geolocation) {
       )
       .then((response) => response.json())
       .then((data) => {
-       console.log(data)
+        renderCurrentWeather(data);
       })
       .catch((err) => alert('Something went wrong, please double-check your URL')) 
     };
+
+    const renderCurrentWeather = (data) => {
+      currentEl.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <h2>Current Conditions</h2>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+        <div class="current">
+          <div class="temp">${((data.main.temp).toFixed(0))}℃</div>
+          <div class="condition">${data.weather[0].description}</div>
+        </div>
+       `);
+     };
 
 
     getCurrentWeather(lat, lon);
