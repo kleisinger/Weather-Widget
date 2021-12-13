79f8bc6187c61efefdc66913d1df021e48003353
Mat Kleisinger <mathew.kleisinger@gmail.com>
2021-12-12 21:24:24 -0600

    gulp minify for CSS and JS added

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const { src, dest, series, parallel } = require('gulp');
+const minify = require('gulp-clean-css');
+const terser = require('gulp-terser');
 
 const defaultTask = () => {
   return src('src/*.html').pipe(dest('dist'));
 };
 
 const scripts = () => {
-  return src('src/*.js').pipe(dest('dist'));
+  return src('src/*.js')
+  .pipe(terser())
+  .pipe(dest('dist'));
 };
 
 const styles = () => {
-  return src('src/*.css').pipe(dest('dist'));
+  return src('src/*.css')
+  .pipe(minify())
+  .pipe(dest('dist'));
 };
-
-
 
 exports.default = defaultTask;
 exports.build = series(defaultTask, scripts, styles);
